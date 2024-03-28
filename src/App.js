4183829178e4190d14ab7250e559657a9179b7c2
ArunKumar <arun@ESTPL.ESSELSYNERG.COM>
2024-03-28 12:45:15 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Components/Nav/Nav';
 import Carousel from './Components/Carousel/Carousel-slick';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Carousel />
       <About />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import './Footer.css'
+function Footer(){
+    return(
+        <div className='footer-container'>
+            <div></div>
+            <div className="contact-links">
+                <div className='phone'><p>123-123-56464</p></div>
+                <div className='website'><p>frutohomedesugn.com</p></div>
+                <div className='instagram'><p>instagram</p></div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+export default Footer;
