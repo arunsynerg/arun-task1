0022398123e19583b681606eb2baa8b7493d84f1
ArunKumar <arun@ESTPL.ESSELSYNERG.COM>
2024-04-10 15:36:29 +0530

    footer added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,17 +1,38 @@
 import './Footer.css'
 function Footer(){
-    return(
-        <div className='footer-container'>
-            <div></div>
-            <div className="contact-links">
-                <div className='phone'><p>123-123-56464</p></div>
-                <div className='website'><p>frutohomedesugn.com</p></div>
-                <div className='instagram'><p>instagram</p></div>
+    return (
+      <>
+        <div className="footer-container">
+          <div></div>
+          <div className="contact-links">
+            <div className="phone">
+              <p>123-123-56464</p>
             </div>
+            <div className="website">
+              <p>frutohomedesugn.com</p>
+            </div>
+            <div className="instagram">
+              <p>instagram</p>
+            </div>
+          </div>
+          <div>
             <div>
-
+              <p>asdf</p>
             </div>
+            <div></div>
+          </div>
+          <div></div>
         </div>
-    )
+        <div className="email-container">
+          <div></div>
+          <div className="email-subscribe">
+            <p className="email-desc">Subscribe for instant updates</p>
+            <input type="text" placeholder="email" className="input" />
+            <button>Subscibe</button>
+          </div>
+          <div></div>
+        </div>
+      </>
+    );
 }
 export default Footer;
